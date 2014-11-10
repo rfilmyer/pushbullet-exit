2e971956ed6761ce1e47dadc2f9c071d0ca683c8
Roger Filmyer <roger.filmyer@gmail.com>
2014-11-10 18:10:48 -0500

    First try at shell script
    This is my first try at making a shell script. Right now I am having
    problems getting it to properly get the exit status from the last
    command sent, so it sends success notifications, whatever the result
    actually is. See http://stackoverflow.com/questions/12999830/getting-exit-code-of-last-shell-command-in-another-script
    for an explanation.

--- pushbullet-exit.sh
+++ pushbullet-exit.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-PREV_EXIT=$(echo $?)
-PREV_CMD=$(!:p)
+# Still trying to figure out how to capture the last command
+# PREV_CMD=$(fc -ln "$1" "$1" | sed '1s/^[[:space:]]*//')
 
-echo "Exit Code" $PREV_EXIT", Previous Command: " $PREV_CMD
-#echo $prev_exit
+PREV_EXIT=$(echo $?)
+API_KEY=$(cat ./api-key)
+
+echo $PREV_EXIT
+
+if [ -z "$PREV_EXIT" ]
+then
+    echo "Did not catch exit status of previous application"
+    exit 1
+fi
+if [ "$PREV_EXIT" -eq "0" ]
+then
+    curl -u $API_KEY: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "title": "Command Successful", "body": "The command you had run on your computer has successfully completed."}' > /dev/null
+else
+    curl -u $API_KEY: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "title": "Command Error '$PREV_EXIT'", "body": "The command you had run on your computer did not exit successfully."}' > /dev/null
+fi
+exit 0
