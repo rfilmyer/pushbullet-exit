0ef67c093644bed14a21eca3a62e38f4357dc3a7
Roger Filmyer <roger.filmyer@gmail.com>
2014-11-11 19:52:24 -0500

    Removed absurd if-statement; 1 gitignore addition
    Even if no previous command had run in bash, $? can never be null.
    I removed an if statement that would therefore never execute and
    only serve to mislead.
    
    As an additional change, I added *.swp to gitignore to make sure
    I don't do anything silly with ctrl-z'ed vim instances.

--- pushbullet-exit.sh
+++ pushbullet-exit.sh
@@ -3,18 +3,17 @@
 # Still trying to figure out how to capture the last command
 # PREV_CMD=$(fc -ln "$1" "$1" | sed '1s/^[[:space:]]*//')
 
+# This always returns 0, can't find out how this works.
 PREV_EXIT=$(echo $?)
+
 API_KEY=$(cat ./api-key)
 
-echo $PREV_EXIT
-
-if [ -z "$PREV_EXIT" ]
-then
-    echo "Did not catch exit status of previous application"
-    exit 1
+# Fallback: execute with ./pushbullet-exit.sh $? to pass error along
+if [ -n "$1" ]; then
+    PREV_EXIT=$1
 fi
-if [ "$PREV_EXIT" -eq "0" ]
-then
+
+if [ "$PREV_EXIT" -eq "0" ]; then
     curl -u $API_KEY: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "title": "Command Successful", "body": "The command you had run on your computer has successfully completed."}' > /dev/null
 else
     curl -u $API_KEY: -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "title": "Command Error '$PREV_EXIT'", "body": "The command you had run on your computer did not exit successfully."}' > /dev/null
